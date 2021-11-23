f9c5601f28aaf5658749f6d28c0280ffe68bbede
Olcay ÇİFTÇİ <olcay_farmer@hotmail.com>
2021-11-24 01:38:03 +0300

    objelere onmousedrag özelliği eklendi

--- Sprite/Assets/Scripts/ClickTracker.cs
+++ Sprite/Assets/Scripts/ClickTracker.cs
@@ -10,4 +10,12 @@ public class ClickTracker : MonoBehaviour
     {
         text.text = this.name;
     }
+    private void OnMouseDrag()
+    {
+        Vector2 pos = Input.mousePosition; //2 boyutlu vektorde farenýn pozisyonunu pos a atadýk.
+        Vector3 objPos = Camera.main.ScreenToWorldPoint(new Vector3(pos.x,pos.y,1f));
+        //ekranda bulunan noktayý sahneye uyarlamak için 3 boyutlu cunku býzým ortamýmýz. ekrandaki noktaya objeyi eklemek için
+
+        transform.position = objPos; //fare týklanýp sürüklendikçe farenin pozisyonu dogrultusunda ekrana obj nin pos unu transformlýyoruz.
+    }
 }
