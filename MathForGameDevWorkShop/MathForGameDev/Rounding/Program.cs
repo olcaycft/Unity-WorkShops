324ab2f27a46c2648bf73c32ca8f8960c2588074
Olcay ÇİFTÇİ <olcay_farmer@hotmail.com>
2022-01-01 21:39:48 +0300

    rounding, ceiling, floor

---
+++ MathForGameDevWorkShop/MathForGameDev/Rounding/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Rounding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number1 = 5.3;
+            double number2 = 5.7;
+            Console.WriteLine("Sayı1= {0}", number1);
+            Console.WriteLine("Rounding Sonucu= {0}", RoundingClass.Round(number1));
+            Console.WriteLine("Ceiling Sonucu= {0}", RoundingClass.Ceil(number1));
+            Console.WriteLine("Floor Sonucu= {0}", RoundingClass.Floor(number1));
+
+            Console.WriteLine("Sayı2= {0}", number2);
+            Console.WriteLine("Rounding Sonucu= {0}", RoundingClass.Round(number2));
+            Console.WriteLine("Ceiling Sonucu= {0}", RoundingClass.Ceil(number2));
+            Console.WriteLine("Floor Sonucu= {0}", RoundingClass.Floor(number2));
+        }
+    }
+}
---
+++ MathForGameDevWorkShop/MathForGameDev/Rounding/RoundingClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rounding
+{
+    static class RoundingClass
+    {
+        public static int Round(double number)
+        {
+            number = Math.Round(number);
+            return (int)number;
+        }
+
+        public static int Ceil(double number)
+        {
+            number = Math.Ceiling(number);
+            return (int)number;
+        }
+
+        public static int Floor(double number)
+        {
+            number = Math.Floor(number);
+            return (int)number;
+        }
+    }
+}
