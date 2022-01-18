d47acb23d59431cc92e434f8a491345692a2e2db
Olcay ÇİFTÇİ <olcay_farmer@hotmail.com>
2022-01-18 16:45:28 +0300

    Add: Fireball particle

--- ParticleEffectsWorkShop/Assets/Scripts/DestinationTracker.cs
+++ ParticleEffectsWorkShop/Assets/Scripts/DestinationTracker.cs
@@ -17,6 +17,10 @@ public class DestinationTracker : MonoBehaviour
 
     private void TrackDestination()
     {
+        Vector3 destinationDirection = enemyPosition - transform.position;
+        float singleStep = 3f * Time.deltaTime;
+        Vector3 newDirection = Vector3.RotateTowards(transform.forward, destinationDirection, singleStep, 0f);
+        transform.rotation = Quaternion.LookRotation(newDirection);
         var pos = transform.position;
         transform.position = Vector3.MoveTowards(pos,
             Vector3.Lerp(pos, enemyPosition, 3f), 3f * Time.deltaTime);
