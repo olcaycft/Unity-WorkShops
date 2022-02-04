b707de9a15c11f029402725e9292b1346c1692bc
Olcay ÇİFTÇİ <olcay_farmer@hotmail.com>
2022-02-05 02:05:11 +0300

    Add: Auto and random obstacle spawn

--- AutoLevelDesign-HC/Assets/Scripts/Plane/PlaneGenerator.cs
+++ AutoLevelDesign-HC/Assets/Scripts/Plane/PlaneGenerator.cs
@@ -1,9 +1,9 @@
-using System;
 using UnityEngine;
 
 public class PlaneGenerator : MonoBehaviour
 {
     [SerializeField] private GameObject planePrefab;
+    [SerializeField] private GameObject obstaclePrefab;
     [SerializeField] private Vector3 startPoint;
     private int planeCount = 10;
 
@@ -14,10 +14,16 @@ public class PlaneGenerator : MonoBehaviour
 
     private void GeneratePlane()
     {
-        for (int i = 0; i <planeCount ; i++)
+        for (int i = 0; i < planeCount; i++)
         {
-            GameObject plane=Instantiate(planePrefab, startPoint, Quaternion.identity);
+            GameObject plane = Instantiate(planePrefab, startPoint, Quaternion.identity);
             startPoint = plane.transform.GetChild(1).transform.position;
+            if (i % 2 == 0)
+            {
+                int randomObstaclePlace = Random.Range(2, 5);
+                Instantiate(obstaclePrefab, plane.transform.GetChild(randomObstaclePlace).transform.position,
+                    Quaternion.identity);
+            }
         }
     }
 }
